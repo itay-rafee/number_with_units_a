97c02adb3e3bcefcbaca0144d23e7a2067371b36
root <root@DESKTOP-5JP3J6T.localdomain>
2021-04-13 15:13:15 +0300

    fix tidy

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -4,18 +4,19 @@
 #include <array>
 #include <sstream>
 #include "NumberWithUnits.hpp"
+#include <cstdio>
 
 using namespace std;
 
 // map<string,string> couples;
-map<string, map<string, float>> units_sign;
+//map<string, map<string, float>> units_sign;
 // map<string,string>::iterator it1;
 const int zero = 0, one = 1, two = 2;
 
 namespace ariel
 {
     /* check if the unit fit */
-    bool contain(string unit1, string unit2)
+    bool contain(const string& unit1, const string& unit2)
     {
         if (units_sign.find(unit1) != units_sign.end())
         {
@@ -27,9 +28,9 @@ namespace ariel
         return false;
     }
 
-    array<string, two> split(string s, string delimiter)
+    array<string, two> split( string& s,  string& delimiter)
     {
-        size_t pos = 0;
+        size_t pos=zero;
         string token;
         if ((pos = s.find(delimiter)) != string::npos)
         {
@@ -40,20 +41,24 @@ namespace ariel
         return {"", ""};
     }
 
-    void set_neighbors(string s1, string s2, float num)
+    void set_neighbors(string& s1, string& s2, float num)
     {
         map<string, float> m = units_sign[s2];
-        for (map<string, float>::iterator it = m.begin(); it != m.end(); ++it)
+        for (auto & it : m)
         {
-            units_sign[s1][it->first] = num * it->second;
+            units_sign[s1][it.first] = num * it.second;
         }
     }
 
     void set_unit(string str)
     {
-        string delimiter_1 = " = ", delimiter_2 = " ";
-        float n1, n2;
-        string token, s1, s2;
+        string delimiter_1 = " = ";
+        string delimiter_2 = " ";
+        float n1 =0;
+        float n2=0;
+        string token;
+        string s1;
+        string s2;
         array<string, two> sp = split(str, delimiter_1);
         array<string, two> sp2 = split(sp.at(zero), delimiter_2);
         stringstream geek(sp2.at(zero));
@@ -68,7 +73,7 @@ namespace ariel
         set_neighbors(s2, s1, n2 / n1);
     }
 
-    static void read_units(ifstream &units_file)
+    void NumberWithUnits::read_units(ifstream &units_file)
     {
         string line;
         while (getline(units_file, line))
@@ -84,7 +89,7 @@ namespace ariel
     }
     istream &operator>>(istream &input, NumberWithUnits &t)
     {
-        char ch;
+        char ch =0;
         return (input >> t._num >> ch >> t._unit >> ch);
     }
     NumberWithUnits operator+=(NumberWithUnits &num_1, const NumberWithUnits &num_2)
@@ -130,15 +135,15 @@ namespace ariel
         return copy;
     }
 
-    const NumberWithUnits operator*(NumberWithUnits &num, float n)
+    NumberWithUnits operator*(NumberWithUnits &num, float n)
     {
         return NumberWithUnits{num._num * n, num._unit};
     }
-    const NumberWithUnits operator*(float n, NumberWithUnits &num)
+    NumberWithUnits operator*(float n, NumberWithUnits &num)
     {
         return NumberWithUnits{num._num * n, num._unit};
     }
-    const NumberWithUnits operator-(const NumberWithUnits &num_1, const NumberWithUnits &num_2)
+    NumberWithUnits operator-(const NumberWithUnits &num_1, const NumberWithUnits &num_2)
     {
         if (contain(num_1._unit, num_2._unit))
         {
@@ -147,11 +152,11 @@ namespace ariel
         }
         throw exception();
     }
-    const NumberWithUnits operator-(const NumberWithUnits &num)
+    NumberWithUnits operator-(const NumberWithUnits &num)
     {
         return NumberWithUnits{-num._num, num._unit};
     }
-    const NumberWithUnits operator+(const NumberWithUnits &num_1, const NumberWithUnits &num_2)
+    NumberWithUnits operator+(const NumberWithUnits &num_1, const NumberWithUnits &num_2)
     {
         if (contain(num_1._unit, num_2._unit))
         {
@@ -160,7 +165,7 @@ namespace ariel
         }
         throw exception();
     }
-    const NumberWithUnits operator+(const NumberWithUnits &num, float n)
+    NumberWithUnits operator+(const NumberWithUnits &num, float n)
     {
         return NumberWithUnits{num._num + n, num._unit};
     }
@@ -168,12 +173,13 @@ namespace ariel
     {
         if (contain(num_1._unit, num_2._unit))
         {
+            bool ans = false;
             float n_2 = units_sign[num_2._unit][num_1._unit] * num_2._num;
             if (num_1._num > n_2)
             {
-                return true;
+                ans = true;
             }
-            return false;
+            return ans;
         }
         throw exception();
     }
@@ -181,12 +187,13 @@ namespace ariel
     {
         if (contain(num_1._unit, num_2._unit))
         {
+            bool ans = false;
             float n_2 = units_sign[num_2._unit][num_1._unit] * num_2._num;
             if (num_1._num >= n_2)
             {
-                return true;
+                ans = true;
             }
-            return false;
+            return ans;
         }
         throw exception();
     }
@@ -194,12 +201,13 @@ namespace ariel
     {
         if (contain(num_1._unit, num_2._unit))
         {
+            bool ans = false;
             float n_2 = units_sign[num_2._unit][num_1._unit] * num_2._num;
             if (num_1._num < n_2)
             {
-                return true;
+                ans = true;
             }
-            return false;
+            return ans;
         }
         throw exception();
     }
@@ -207,12 +215,13 @@ namespace ariel
     {
         if (contain(num_1._unit, num_2._unit))
         {
+            bool ans = false;
             float n_2 = units_sign[num_2._unit][num_1._unit] * num_2._num;
             if (num_1._num <= n_2)
             {
-                return true;
+                ans = true;
             }
-            return false;
+            return ans;
         }
         throw exception();
     }
@@ -220,12 +229,13 @@ namespace ariel
     {
         if (contain(num_1._unit, num_2._unit))
         {
+            bool ans = false;
             float n_2 = units_sign[num_2._unit][num_1._unit] * num_2._num;
             if (num_1._num == n_2)
             {
-                return true;
+                ans = true;
             }
-            return false;
+            return ans;
         }
         throw exception();
     }
@@ -233,16 +243,55 @@ namespace ariel
     {
         if (contain(num_1._unit, num_2._unit))
         {
+            bool ans = false;
             float n_2 = units_sign[num_2._unit][num_1._unit] * num_2._num;
             if (num_1._num != n_2)
             {
-                return true;
+                ans = true;
             }
-            return false;
+            return ans;
         }
-        return false;
 
         throw exception();
     }
 
+
 }
+
+// using namespace ariel;
+// int main() {
+//     char filename[] = "units.txt";
+// //    ofstream units_f(filename);
+// //    units_f << "1 km = 1000 m\n1 m = 100 cm\n1 kg = 1000 g\n1 ton = 1000 kg\n1 hour = 60 min\n1 min = 60 sec\n1 USD = 3.33 ILS";
+// //    units_f.close();
+//     ifstream units_file{filename};
+//     NumberWithUnits::read_units(units_file);
+
+//     NumberWithUnits a{2, "km"};   // 2 kilometers
+//     cout << a << endl;           // Prints "2[km]".
+//     cout << (-a) << endl;    // Prints "-2[km]"
+//     cout << (3*a) << endl;    // Prints "6[km]"
+
+//     NumberWithUnits b{300, "m"};  // 300 meters
+//     cout << (a+b) << endl;   // Prints "2.3[km]". Note: units are determined by first number (a).
+//     cout << (b-a) << endl;   // Prints "-1700[m]". Note: units are determined by first number (b).
+
+//     cout << boolalpha; // print booleans as strings from now on:
+//     cout << (a>b) << endl;  // Prints "true"
+//     cout << (a<=b) << endl;  // Prints "false"
+//     cout << (a==NumberWithUnits{2000, "m"}) << endl;  // Prints "true"
+
+//     istringstream sample_input{"700 [ kg ]"};
+//     sample_input >> a;
+//     cout << a << endl;   // Prints "700[kg]"
+//     cout << (a += NumberWithUnits{1, "ton"}) << endl;  // prints "1700[kg]"
+//     cout << a << endl;   // Prints "1700[kg]". Note that a has changed.
+
+//     try {
+//         cout << (a+b) << endl;
+//     } catch (const std::exception& ex) {
+//         cout << ex.what() << endl; // Prints "Units do not match - [m] cannot be converted to [kg]"
+//     }
+//     cout << "End of demo!" << endl;
+//     return 0;
+// }
--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -1,34 +1,41 @@
 #include <iostream>
+#include <utility>
+#include <map>
 
 using namespace std;
 
 namespace ariel
 {
+    static map<string, map<string, float>> units_sign;
+
     class NumberWithUnits
     {
     private:
+//        float _n;
+//        string _s;
+
+    public:
         float _num;
         string _unit;
 
-    public:
-        NumberWithUnits(float n, string s): _num(n),_unit(s){}
+        NumberWithUnits(float n, string s): _num(n),_unit(std::move(s)){}
         static void read_units(ifstream &units_file);
         friend ostream &operator<<(ostream &out, const NumberWithUnits &t);
         friend istream &operator>>(istream &in, NumberWithUnits &t);
-        friend NumberWithUnits operator+=(NumberWithUnits &num_1, const NumberWithUnits &num_2);
-        friend NumberWithUnits operator-=(NumberWithUnits &num_1, const NumberWithUnits &num_2);
-        friend NumberWithUnits operator++(NumberWithUnits &num);
-        friend NumberWithUnits operator++(NumberWithUnits &num, int);
+        friend  NumberWithUnits operator+=(NumberWithUnits &num_1, const NumberWithUnits &num_2);
+        friend  NumberWithUnits operator-=(NumberWithUnits &num_1, const NumberWithUnits &num_2);
+        friend  NumberWithUnits operator++(NumberWithUnits &num);
+        friend  NumberWithUnits operator++(NumberWithUnits &num, int);
         friend NumberWithUnits operator--(NumberWithUnits &num);
-        friend NumberWithUnits operator--(NumberWithUnits &num, int);
-        friend NumberWithUnits operator*=(NumberWithUnits &num, float n);
-        friend NumberWithUnits operator*=(float n, NumberWithUnits &num);
-        friend const NumberWithUnits operator*(NumberWithUnits &num, float n);
-        friend const NumberWithUnits operator*(float n, NumberWithUnits &num);
-        friend const NumberWithUnits operator-(const NumberWithUnits &num_1, const NumberWithUnits &num_2);
-        friend const NumberWithUnits operator-(const NumberWithUnits &num);
-        friend const NumberWithUnits operator+(const NumberWithUnits &num_1, const NumberWithUnits &num_2);
-        friend const NumberWithUnits operator+(const NumberWithUnits &num, float n);
+        friend  NumberWithUnits operator--(NumberWithUnits &num, int);
+        friend  NumberWithUnits operator*=(NumberWithUnits &num, float n);
+        friend  NumberWithUnits operator*=(float n, NumberWithUnits &num);
+        friend NumberWithUnits operator*(NumberWithUnits &num, float n);
+        friend NumberWithUnits operator*(float n, NumberWithUnits &num);
+        friend NumberWithUnits operator-(const NumberWithUnits &num_1, const NumberWithUnits &num_2);
+        friend NumberWithUnits operator-(const NumberWithUnits &num);
+        friend NumberWithUnits operator+(const NumberWithUnits &num_1, const NumberWithUnits &num_2);
+        friend NumberWithUnits operator+(const NumberWithUnits &num, float n);
         friend bool operator>(const NumberWithUnits &num_1, const NumberWithUnits &num_2);
         friend bool operator>=(const NumberWithUnits &num_1, const NumberWithUnits &num_2);
         friend bool operator<(const NumberWithUnits &num_1, const NumberWithUnits &num_2);
